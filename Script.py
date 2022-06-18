21980b55a6dbe79cab2f516e378c25c785214b9c
Abhyudak <105523705+Abhyudak@users.noreply.github.com>
2022-06-18 22:24:51 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,22 +1,26 @@
 class Script(object):
     START_TXT = """𝗛𝗲𝗹𝗹𝗼 {},
 𝗠𝘆 𝗻𝗮𝗺𝗲 𝗶𝘀  <a href=https://t.me/{}>{}</a>!
-ഞാൻ ഒരു 𝐀𝐔𝐓𝐎-𝐅𝐈𝐋𝐓𝐄𝐑-𝐁𝐎𝐓 𖠌 ആണ്, എന്റെ മുതലാളി <a href='https://t.me/AKLucashood'>༒ 🄰🄺🅅 666 ༒🕊️⃟࿐</a> ആണ്, നിങ്ങൾക്ക് ബിരിയാണി വേണോ..."""
+ഞാൻ ഒരു 𝐀𝐔𝐓𝐎-𝐅𝐈𝐋𝐓𝐄𝐑-𝐁𝐎𝐓 𖠌 ആണ്, എന്റെ മുതലാളി <a href='https://t.me/AKLucashood'>༒ 🄰🄺🅅 666 ༒🕊️⃟࿐</a> ആണ്, നിങ്ങൾക്ക് ബിരിയാണി വേണോ...
+✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵
+©MᴀɪɴᴛᴀɪɴᴇD Bʏ:<a href='https://t.me/AKLucashood'>༒ 🄰🄺🅅 666 ༒🕊️⃟࿐</a>"""
 
     HELP_TXT = """Hey {}
 <b>Here Is The Help For My Commands.</b>"""
 
-    ABOUT_TXT = """
-╭━━━━━━━━━━━━━━━━━━━━━➣ 
-┣⪼  ᴍʏ ɴᴀᴍᴇ: {}
-┣⪼ ᴄʀᴇᴀᴛᴏʀ: <a href='https://t.me/AKLucashood'>༒ 🄰🄺🅅 666 ༒🕊️⃟࿐</a>
-┣⪼ Dev 2  :<a href='https://t.me/ToxicboymX'>🅃🄾🅇🄸🄲 ⒷⓄⓎ🕊️⃟࿐</a>
-┣⪼ ʟɪʙʀᴀʀʏ: <a href='https://docs.pyrogram.org/'>Pyrogram</a>
-┣⪼ 𝚁𝚎𝚙𝚘: <a href='https://t.me/AkvMovieHub'>ᴄʟɪᴄᴋ</a>
-┣⪼ ᴅᴀᴛᴀ ʙᴀꜱᴇ: <a href='https://t.me/AkvMovieHub'>Private DB</a>
-┣⪼ 𖠌ʙᴏᴛ ꜱᴇʀᴠᴇʀ: <a href='https://t.me/AkvMovieHub'>Private Server</a>
-┣⪼ ʙᴜɪʟᴅ ꜱᴛᴀᴛᴜꜱ: v2.0.1 [ Beta ]
-╰━━━━━━━━━━━━━━━━━━━━━➣╚══════════════════❍⊱❁۪۪
+    ABOUT_TXT = """  
+╔════❰ ꪖ᥇ꪮꪊ𝓽 ꪑ𝘴ᧁ ❱═❍⊱❁۪۪      
+║╭━━━━━━━━━━━━━━━━━━━━━➣ 
+║┣⪼  ᴍʏ ɴᴀᴍᴇ: {}
+║┣⪼ ᴄʀᴇᴀᴛᴏʀ: <a href='https://t.me/AKLucashood'>༒ 🄰🄺🅅 666 ༒🕊️⃟࿐</a>
+║┣⪼ Dev 2  :<a href='https://t.me/ToxicboymX'>🅃🄾🅇🄸🄲 ⒷⓄⓎ🕊️⃟࿐</a>
+║┣⪼ ʟɪʙʀᴀʀʏ: <a href='https://docs.pyrogram.org/'>Pyrogram</a>
+║┣⪼ 𝚁𝚎𝚙𝚘: <a href='https://t.me/AkvMovieHub'>ᴄʟɪᴄᴋ</a>
+║┣⪼ ᴅᴀᴛᴀ ʙᴀꜱᴇ: <a href='https://t.me/AkvMovieHub'>Private DB</a>
+║┣⪼ 𖠌ʙᴏᴛ ꜱᴇʀᴠᴇʀ: <a href='https://t.me/AkvMovieHub'>Private Server</a>
+║┣⪼ ʙᴜɪʟᴅ ꜱᴛᴀᴛᴜꜱ: v2.0.1 [ Beta ]
+║╰━━━━━━━━━━━━━━━━━━━━━➣
+╚══════════════════❍⊱❁۪۪
 
 
 
